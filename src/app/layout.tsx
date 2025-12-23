3df8af6048e7142d59ad837ec2de5041f4f8d791
ygtrkn-lab <yigitraiken@gmail.com>
2025-12-23 22:51:46 +0300

    site guncellendi

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -237,7 +237,6 @@ export default function RootLayout({
                 s.parentNode.insertBefore(t,s)}(window, document,'script',
                 'https://connect.facebook.net/en_US/fbevents.js');
                 fbq('init', '${META_PIXEL_ID}');
-                fbq('set', 'autoConfig', false, '${META_PIXEL_ID}');
                 fbq('track', 'PageView');
               `}
             </Script>
